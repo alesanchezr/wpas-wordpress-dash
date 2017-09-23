74b1bd68a55dfc3d3f8d9e5a3a5698d205eed1d8
Alejandro Sanchez <aalejo@gmail.com>
2017-09-23 23:54:18 +0000

    integracion con pll plugin

--- src/WPAS/Language/WPASLanguages.php
+++ src/WPAS/Language/WPASLanguages.php
@@ -3,24 +3,39 @@
 namespace WPAS\Language;
 
 use WPAS\Utils\HelperStringFormat;
-use WPAS\Utils\Exception;
+use WPAS\Utils\WPASException;
 
 class WPASLanguages{
 	
 	public static $currentLanguage = null;
+	private static $languagesDirectory = null;
+	private static $isAdmin = true;
 	
-	function __construct() {
-
-
+	function __construct($settings) {
+		
+		//self::$isAdmin = is_admin();
+		self::$isAdmin = false;
+		if(!self::$isAdmin)
+		{
+			if(!empty($settings['languages-directory'])) 
+				self::$languagesDirectory = $settings['languages-directory'];
+		
+			$this->activate();	
+		}
 	}
 	
 	private function activate(){
-		if (function_exists( 'pll_register_string' ) )
+		if (function_exists( 'pll_register_string' ))
 		{
-			self::$currentLanguage = require(ABSPATH.'wp-content/themes/thedocs-child/src/php/languages/lang.en.php');
-			if(!self::$currentLanguage) throw new Exception("Unable to load language json from ".$languageURL, 1);
+			if(empty(self::$languagesDirectory)) throw new WPASException('You need to specify a laguages directory');
+			
+			self::$currentLanguage = require(self::$languagesDirectory.'en.lang.php');
+			if(!self::$currentLanguage) throw new WPASException("Unable to load language array from ".$languageURL, 1);
 
+			if(empty(self::$currentLanguage['labels']))  throw new WPASException("The language array must have a 'labels' key with the list of key->value strings to register", 1);
+			
 			$languageArray = self::$currentLanguage['labels'];
+			//print_r($languageArray); die();
 			foreach ($languageArray as $key => $value) {
 				pll_register_string($key, $value);
 			}
