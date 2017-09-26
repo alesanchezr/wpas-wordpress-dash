1a8dd87711f572f511b6a815deb2f34090bae213
Alejandro Sanchez <aalejo@gmail.com>
2017-09-26 22:20:51 +0000

    no idea

--- src/WPAS/Settings/wpas-themesettingsbuilder.js
+++ src/WPAS/Settings/wpas-themesettingsbuilder.js
@@ -96,7 +96,7 @@ jQuery( document ).ready(function() {
     }
   };
 
-jQuery('.add-new-option').click(function(e){
+  jQuery('.add-new-option').click(function(e){
     
     var inputValue = jQuery(this).siblings('input').val();
     jQuery.ajax({
@@ -125,6 +125,32 @@ jQuery('.add-new-option').click(function(e){
       success: function(data){
         if(data.code==200) location.reload();
         else alert('Something went wrong: '+data.message);
+      },
+      error: function(p1,p2,p3){
+        console.log(p3);
+      }
+    });
+    e.preventDefault();
+  });
+  
+  jQuery('.wpas-settings-button').click(function(e){
+    
+    var hookName = jQuery(this).data('hook');
+    jQuery.ajax({
+      url: 'admin-ajax.php',
+      method: "post",
+      dataType: 'json',
+      data: {action:"ajax_theme_option", hook: hookName, function: "wpas_do_action"},
+      success: function(data){
+        if(data){
+          if(data.hasOwnProperty('code')){
+            if(data.code==200) location.reload();
+            else alert('Something went wrong: '+data.message);
+          }
+          else{
+            alert('Invalid AJAX answer, check that you are not printing anything in your closure');
+          }
+        }
       },
       error: function(p1,p2,p3){
         console.log(p3);
