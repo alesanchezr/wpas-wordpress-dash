d57254d4cc8d77c8b3643622ff7536639226b205
Ignacio Cordoba <icordoba@4geeks.co>
2018-04-06 19:24:08 -0400

    Update BasePostType.php
    Updated to use the same slug as in the CPT definition (functions.php)

--- src/WPAS/Types/BasePostType.php
+++ src/WPAS/Types/BasePostType.php
@@ -24,22 +24,30 @@ class BasePostType extends PostType{
     public function initialize(){}
     public function populate_fields(){}
     
+    public static function setType($theType){
+        self::$postType = $theType;
+    }
+    
     public static function all($args=[], $hook=null){
-
+        $realPostType = get_called_class();
+        $realPostType = strtolower(preg_replace( "%[A-Za-z]\w+\\\%", '',$realPostType));
+        
         if(empty(self::$postType)) throw new WPASException('Please register() the class '.get_called_class().' at least one time before using it');
-        $args = array_merge($args,[
-            'post_type' => self::$postType
-            ]);
+        $args = array_merge([
+            'post_type' => $realPostType
+            ],$args);
         
         $query = new WP_Query($args);
         return $query;
     }
     
     public static function get($args){
+        $realPostType = get_called_class();
+        $realPostType = strtolower(preg_replace( "%[A-Za-z]\w+\\\%", '',$realPostType));
         
-        if(empty(self::$postType)) throw new WPASException('Please instanciete the class '.get_called_class().' at least one time before using it');
-        if(is_array($args)) $args = array_merge($args,[ 'post_type' => self::$postType ]);
-        else if(is_numeric($args)) $args = [ 'post_type' => self::$postType, 'ID' => $args ];
+        if(empty(self::$postType)) throw new WPASException('Please instanciate the class '.get_called_class().' at least one time before using it');
+        if(is_array($args)) $args = array_merge($args,[ 'post_type' => $realPostType ]);
+        else if(is_numeric($args)) $args = [ 'post_type' => $realPostType, 'ID' => $args ];
         
         $query = new WP_Query($args);
         if($query->posts && $query->post_count >=1){
@@ -48,11 +56,13 @@ class BasePostType extends PostType{
     }
     
     public static function getBySlug($slug){
+        $realPostType = get_called_class();
+        $realPostType = strtolower(preg_replace( "%[A-Za-z]\w+\\\%", '',$realPostType));
         
         if(empty(self::$postType)) throw new WPASException('Please instanciete the class '.get_called_class().' at least one time before using it');
         if(!is_string($slug)) throw new WPASException('getBySlug must receive a string as parameter $slug');
         
-        $args = [ 'post_type' => self::$postType, 'name' => $slug ];
+        $args = [ 'post_type' => $realPostType, 'name' => $slug ];
         $query = new WP_Query($args);
         if($query->posts && $query->post_count >=1){
             return $query->posts[0];
